c7d21b86d832063c95566530b965f765fd01fe2b
S <s>
2020-11-20 16:32:47 +0400

    {"types":"C","runs":13}

--- BlankClass.java
+++ BlankClass.java
@@ -21,7 +21,7 @@ public class BlankClass extends ConsoleProgram {
 		Character lett = ' ';
 		String str = readLine("Enter text: ");
 		for (int i = 0; i < str.length()-1; i++) {
-				for (int j = i + 1; j < str.length(); j++) {
+				for (int j = i ; j < str.length(); j++) {
 					if (str.charAt(i) == str.charAt(j)) {
 						temp++;
 					}
