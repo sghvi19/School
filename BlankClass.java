bfdabfaaa7862fe105054e4b10fc642f2e7b8d58
S <s>
2020-11-25 01:32:04 +0400

    {"types":"C","runs":1}

--- BlankClass.java
+++ BlankClass.java
@@ -7,22 +7,66 @@
  * Then you can extend GraphicsProgram, ConsoleProgram, or DialogProgram as you like.
  */
 
+import java.awt.Color;
 import java.awt.event.MouseEvent;
 import java.util.StringTokenizer;
 
 import acm.graphics.GLine;
+import acm.graphics.GObject;
 import acm.graphics.GOval;
 import acm.program.*;
+import acm.util.RandomGenerator;
 
-public class BlankClass extends ConsoleProgram {
-
-	public void run() {
+public class BlankClass extends GraphicsProgram {
+	private static final double CIRCLE_D = 60;
+	private RandomGenerator rand = RandomGenerator.getInstance();
+	private GOval gOval = null;
+	private static final int DELAY = 100;
 	
+	public void run() {
+		setup();
 		
-		String str= readLine(" something: ");
-		StringTokenizer tok=new StringTokenizer(str," ");
-		while(tok.hasMoreTokens()) {
-			println(tok.nextToken());
+		while(true) {
+			
+			if(gOval != null && gOval.getFillColor()!= Color.GREEN) {
+				gOval.setColor(getRandomColor());
+				pause(DELAY);
+			}
+		}
+	}
+	
+	private void setup() {
+		addMouseListeners();
+	}
+	
+	public void mouseClicked(MouseEvent e) {
+		double x = e.getX();
+		double y = e.getY();
+		
+		GObject obj = getElementAt(x,y);
+		if( obj == null) {
+			GOval gOval = new GOval(x - CIRCLE_D / 2, y - CIRCLE_D / 2, CIRCLE_D, CIRCLE_D);
+			gOval.setFilled(true);
+			gOval.setColor(rand.nextColor());
+			add(gOval);
+		}else {
+		    gOval = (GOval) obj;
+		}
+	}
+	
+	private Color getRandomColor() {
+		int index = rand.nextInt(1, 5);
+		
+		if(index == 1) {
+			return Color.GREEN;
+		}else if(index == 2) {
+			return Color.RED;
+		}else if (index ==3) {
+			return Color.BLUE;
+		}else if(index ==4) {
+			return Color.BLACK;
+		}else {
+			return Color.YELLOW;
 		}
 	}
 }
