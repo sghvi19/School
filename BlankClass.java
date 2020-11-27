e1fc6f4c40a4a61511af85c52f72c093ced645ac
S <s>
2020-11-27 21:32:17 +0400

    {"types":"C","runs":29}

--- BlankClass.java
+++ BlankClass.java
@@ -52,10 +52,10 @@ public class BlankClass extends GraphicsProgram {
 		}
 	}
 
-	public void mouseMoved(MouseEvent e) {
-		mouseX = e.getX();
-		mouseY = e.getY();
-	}
+//	public void mouseMoved(MouseEvent e) {
+//		mouseX = e.getX();
+//		mouseY = e.getY();
+//	}
 
 	private Color getRandomColor() {
 		return rgen.nextColor();
@@ -66,10 +66,10 @@ public class BlankClass extends GraphicsProgram {
 		double yCentre = getHeight() / 2.0;
 		counter++;
 		//println(counter);
-		//if (counter % 2 == 0) {
+		if (counter % 2 == 0) {
 			mouseX = e.getX();
 			mouseY = e.getY();
-	//	}
+		}
 
 		double startDistance = getDistance(xCentre, yCentre, mouseX, mouseY);
 		double endDistance = getDistance(xCentre, yCentre, e.getX(), e.getY());
