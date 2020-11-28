d84afdfe0a7c51e6c6d4d488eaaf6664bcdcd459
S <s>
2020-11-28 20:27:13 +0400

    {"types":"C","runs":21}

--- BlankClass.java
+++ BlankClass.java
@@ -25,19 +25,19 @@ public class BlankClass extends ConsoleProgram {
 	public void run() {
 		String str1 = readLine("Enter: ");
 		String str2 = readLine("Enter: ");
-		
-		for (int i = 0; i < str1.length(); i++) {
-				if(str2.indexOf(str1.charAt(i))!=-1 && str2.indexOf(str1.charAt(i))!= i ){
-					str2= str2.substring(0,str2.indexOf(str1.charAt(i)))+str2.substring(str2.indexOf(str1.charAt(i))+1,str2.length());
-					println(str2);
-				}else if(str2.indexOf(str1.charAt(i))!=-1 &&str2.indexOf(str1.charAt(i))== i) {
-					str2= str2.substring(0,str2.indexOf(str1.charAt(i)));
-					println(str2);
-				}else {
-					println(false);
-					break;
-				}
-		}
+		println(str1.substring(0,0)+str1.substring(1,str1.length()));
+//		for (int i = 0; i < str1.length(); i++) {
+//				if(str2.indexOf(str1.charAt(i))!=-1 && str2.indexOf(str1.charAt(i))!= i ){
+//					str2= str2.substring(0,str2.indexOf(str1.charAt(i)))+str2.substring(str2.indexOf(str1.charAt(i))+1,str2.length());
+//					println(str2);
+//				}else if(str2.indexOf(str1.charAt(i))!=-1 &&str2.indexOf(str1.charAt(i))== i) {
+//					str2= str2.substring(0,str2.indexOf(str1.charAt(i)));
+//					println(str2);
+//				}else {
+//					println(false);
+//					break;
+//				}
+//		}
 		println(true);
 	}
 }
