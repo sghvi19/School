5fc910d988cf3645246a48d434f223505e096b31
S <s>
2020-12-01 22:35:47 +0400

    {"types":"C","runs":5}

--- BlankClass.java
+++ BlankClass.java
@@ -23,27 +23,44 @@ import acm.util.RandomGenerator;
 import acm.graphics.*;
 import acm.program.ConsoleProgram;
 
-public class BlankClass extends ConsoleProgram {
-
-	public void run() {
-		String word = readLine("word:");
-
-		for (int i = 0; i < word.length(); i++) {
-			for (int j = 1; j < word.length(); j++) {
-
-				int result = 1;
-				if (word.charAt(i) != word.charAt(i + j)) {
-					break;
-				}
-				if (word.charAt(i) == word.charAt(i + j)) {
-					result++;
-				}
-				println(result + word.charAt(i));
-
+public class BlankClass extends GraphicsProgram{
+		private static final int RECT_WIDTH = 80;
+		private static final int RECT_HEIGHT = 100;
+		private static final int MIN_H = 10;
+		private RandomGenerator rg;
+		private GOval oval;
+		private GPoint p;
+		private boolean isPressed = false;
+		
+		public void run() {
+			rg = RandomGenerator.getInstance();
+			oval = new GOval(getWidth() / 2 - RECT_WIDTH / 2,
+								getHeight() / 2 - RECT_HEIGHT / 2, 
+							RECT_WIDTH, RECT_HEIGHT);
+			oval.setFilled(true);
+			add(oval);
+			addMouseListeners();
+		}
+		
+		public void mouseClicked(MouseEvent e) {
+			GObject o = getElementAt(e.getX(), e.getY());
+			if(o != null) {
+				oval.setColor(rg.nextColor());
 			}
 		}
+		public void mousePressed(MouseEvent e) {
+			p = new GPoint(e.getX(), e.getY());
+		}
+		
+		
+		public void mouseDragged(MouseEvent e) {
+			double diffX = e.getX() - p.getX();
+			double diffY = e.getY()- p.getY();
+			oval.setSize(oval.getWidth() + 2*diffX, oval.getHeight() + 2*diffY);
+			oval.setLocation(oval.getX() - diffX, oval.getY() - diffY);
+			p.setLocation(e.getX(), e.getY());
+		}
 	}
-}
 // mesame varianti 2 amocana 3
 //	public void run() {
 //		boolean t=true;
