39d9f0fe7db7edb616c70a6a10b565668c078680
S <s>
2020-12-01 22:52:42 +0400

    {"types":"C","runs":14}

--- BlankClass.java
+++ BlankClass.java
@@ -30,7 +30,7 @@ public class BlankClass extends ConsoleProgram {
 		while (doubledElements(str)) {
 			for (int i = 0; i < str.length()-1; i++) {
 				if(str.charAt(i)==str.charAt(i+1)) {
-					str= str.substring(0,i)+str.substring(i+1);
+					str= str.substring(0,i)+str.substring(i+2);
 				}
 			}
 		}
