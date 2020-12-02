0348b0330afd4d2beae5cf09fe7123bef2f126ac
S <s>
2020-12-02 17:47:30 +0400

    {"types":"C","runs":13}

--- BlankClass.java
+++ BlankClass.java
@@ -34,11 +34,12 @@ public class BlankClass extends ConsoleProgram {
 					int n = str.charAt(i) - '0';
 					for (int j = 0; j < n; j++) {
 						result+=str.charAt(i+1);
-					//	str=str.substring(i+2);
+						str=str.substring(i+2);
 						println("b");
 					}
 				}else {
 					result+=str.charAt(i);
+					str=str.substring(i+1);
 					println("c");
 				}
 			}
