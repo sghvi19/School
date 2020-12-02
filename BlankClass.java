e308baf1c21b619fa88f54efb948c36e6ef1c319
S <s>
2020-12-02 20:32:26 +0400

    {"types":"C","runs":62}

--- BlankClass.java
+++ BlankClass.java
@@ -44,15 +44,14 @@ public class BlankClass extends GraphicsProgram {
 	}
 
 	public void mouseClicked(MouseEvent e) {
-		if(getElementAt(e.getX(),e.getY())==null && !t) {
+		if(getElementAt(e.getX(),e.getY())==null) {
 			oval=new GOval(e.getX()-CIRCLE_D/2,e.getY()-CIRCLE_D/2,CIRCLE_D,CIRCLE_D);
 			oval.setFilled(true);
 			oval.setFillColor(rgen.nextColor());
 			add(oval);
 			println("no");
-			t=true;
 		}
-		if(getElementAt(e.getX(),e.getY())!=null && t){
+		if(getElementAt(e.getX(),e.getY())!=null){
 			oval= (GOval)getElementAt(e.getX(),e.getY());
 			t=true;
 			println("yes");
