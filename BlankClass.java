a2856ee2b48e37c81037da5d48e7c15c6021f14a
S <s>
2020-12-06 17:50:48 +0400

    {"types":"C","runs":11}

--- BlankClass.java
+++ BlankClass.java
@@ -54,14 +54,16 @@ public class BlankClass extends ConsoleProgram {
 					
 					ch[r][c] = result.charAt(0);
 					result = result.substring(1);
+					println(ch[r][c]);
 				}
 				if (result.length() == 1) {
 					ch[r][c] = result.charAt(0);
 					result = "";
+					println(ch[r][c]);
 				}
 			}
 		}
-		println(ch[i][i+1]);
+		
 		for (int c = 0; c < i + 1; c++) {
 			for (int r = 0; r < i; r++) {
 				if (r != i - 1) {
