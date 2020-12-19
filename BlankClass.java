d2509971a862621fbbea97e4815cc669dd7af1a9
S <s>
2020-12-19 20:43:01 +0400

    {"types":"C","runs":8}

--- BlankClass.java
+++ BlankClass.java
@@ -31,7 +31,7 @@ import acm.util.RandomGenerator;
 import acm.graphics.*;
 import acm.program.ConsoleProgram;
 
-public class BlankClass extends ConsoleProgram {
+public class BlankClass extends GraphicsProgram {
 	private JTextField field;
 	private GCanvas canvas;
 	public void run() {
