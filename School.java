b581134102b1ff92307bb12aa2989d532a2e2ad8
S <s>
2020-12-31 21:52:37 +0400

    {"types":"C","runs":4}

--- School.java
+++ School.java
@@ -1,4 +1,17 @@
-public class School {
+import java.util.ArrayList;
+
+import acm.program.ConsoleProgram;
+
+public class School extends ConsoleProgram {
+	private ArrayList<String> teachers;
+	public 
+	School() {
+		
+	}
 	
+	public void addTeacher(String teacher) {
+		teachers=new ArrayList<String>();
+		teachers.add(teacher);
+	}
 
 }
