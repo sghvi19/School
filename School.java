830182c55aa33f4b36368152865937b23bda92cb
S <s>
2021-01-04 22:07:16 +0400

    {"types":"C","runs":0}

--- School.java
+++ School.java
@@ -1,17 +1,30 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import acm.program.ConsoleProgram;
 
 public class School extends ConsoleProgram {
-	private ArrayList<String> teachers;
-	public 
-	School() {
-		
+	private HashMap<String, HashMap<String, ArrayList<String>>> teachers;
+
+	public School() {
+		teachers = new HashMap<String, HashMap<String, ArrayList<String>>>();
 	}
-	
+
 	public void addTeacher(String teacher) {
-		teachers=new ArrayList<String>();
-		teachers.add(teacher);
+		if (!teachers.containsKey(teacher)) {
+			HashMap<String,ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+			teachers.put(teacher, map);
+		}
+	}
+
+	public void addSubject(String teacher, String subject) {
+		if(teachers.containsKey(teacher)) {
+			HashMap<String,ArrayList<String>> mp=new HashMap<String,ArrayList<String>>();
+			if(map.)
+			
+			
+			
+		}
 	}
 
 }
