a44944fac93979439de0fd3f42368c6386b88fb3
S <s>
2021-01-05 15:09:46 +0400

    {"types":"C","runs":3}

--- School.java
+++ School.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 import acm.program.ConsoleProgram;
 
@@ -12,18 +13,63 @@ public class School extends ConsoleProgram {
 
 	public void addTeacher(String teacher) {
 		if (!teachers.containsKey(teacher)) {
-			HashMap<String,ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+			HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
 			teachers.put(teacher, map);
 		}
 	}
 
 	public void addSubject(String teacher, String subject) {
+		if (teachers.containsKey(teacher)) {
+			HashMap<String, ArrayList<String>> mp = teachers.get(teacher);
+			ArrayList<String> list = new ArrayList<String>();
+			mp.put(subject, list);
+
+		}
+	}
+
+	public void addPupil(String pupil, String subject) {
+		for (String teacher : teachers.keySet()) {
+			if (teachers.get(teacher).containsKey(subject)) {
+				teachers.get(teacher).get(subject).add(pupil);
+			}
+
+		}
+
+	}
+
+	public Iterator<String> getTeachers(String pupil) {
+		ArrayList<String> list = new ArrayList<String>();
+		for (String teacher : teachers.keySet()) {
+			HashMap<String, ArrayList<String>> map = teachers.get(teacher);
+			for (String subject : map.keySet()) {
+				if (map.get(subject).contains(pupil)) {
+					list.add(teacher);
+					break;
+				}
+			}
+		}
+
+		if (!list.isEmpty()) {
+			return null;
+		}
+		return list.iterator();
+	}
+
+	public Iterator<String> getPupils(String teacher) {
+		ArrayList list = new ArrayList<String>();
+		if (teachers.containsKey(teacher)) {
+			for (String subject : teachers.get(teacher).keySet()) {
+				for (String student : teachers.get(teacher).get(subject)) {
+					list.add(student);
+				}
+			}
+			return list.iterator();
+		}
+		return null;
+	}
+	public void removeTeacher(String teacher) {
 		if(teachers.containsKey(teacher)) {
-			HashMap<String,ArrayList<String>> mp=new HashMap<String,ArrayList<String>>();
-			if(map.)
-			
-			
-			
+			teachers.remove(teacher);
 		}
 	}
 
