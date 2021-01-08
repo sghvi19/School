4ae239c79615e941248d7d5688a678487f0ad21b
S <s>
2021-01-09 02:38:27 +0400

    {"types":"C","runs":7}

--- BlankClass.java
+++ BlankClass.java
@@ -90,6 +90,7 @@ public class BlankClass extends ConsoleProgram {
         String n=arr[0];
         for(int i=0; i<n.length();i++){
             String s=Character.toString(arr[i].charAt(i));
+            println(s);
             for(int j=0; j<arr.length; j++){
                 if(arr[j].contains(s)){
                 number++;
