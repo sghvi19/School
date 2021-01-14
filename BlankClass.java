c221d6e53bda9778caaa24f72cecef2362abf322
S <s>
2021-01-14 04:27:58 +0400

    {"types":"C","runs":32}

--- BlankClass.java
+++ BlankClass.java
@@ -79,6 +79,7 @@ public class BlankClass extends ConsoleProgram {
             result=Character.toString(s.charAt(i))+result;
         }
         println(result);
+       println( s.substring(start,end+1));
             if(s.substring(start,end+1).equals(result))return true;
                                        return false;
     }
