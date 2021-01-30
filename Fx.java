c59b700ed70d958afa191299422bb208213a8b84
S <s>
2021-01-30 23:38:16 +0400

    {"types":"C","runs":41}

--- Fx.java
+++ Fx.java
@@ -1,4 +1,36 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.StringTokenizer;
 
 public class Fx {
+	private String url;
+	private String contest;
+	private ArrayList<String> list;
+	private String keys;
+	public Fx(String line) {
+		StringTokenizer tok = new StringTokenizer(line, " []");
+		list=new ArrayList<String>();
+		url=tok.nextToken();
+		contest=tok.nextToken();
+		keys=tok.nextToken();
+		
+		StringTokenizer tk=new StringTokenizer(keys,",");
+		while(tk.hasMoreTokens()) {
+			list.add(tk.nextToken());
+		}
+		
 
+	}
+
+	public String getURL() {
+		return url;
+	}
+
+	public String getContent() {
+		return contest;
+	}
+
+	public Iterator<String> getKeywords(){
+		return list.iterator();
+	}
 }
