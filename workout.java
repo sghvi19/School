c716a6654b2b6918bb5f8af5b3688decf5e23bc5
S <s>
2021-02-03 01:31:41 +0400

    {"types":"C","runs":49}

--- workout.java
+++ workout.java
@@ -1,23 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import acm.program.ConsoleProgram;
+import acm.util.ErrorException;
 
 public class workout extends ConsoleProgram{
 
 
 	public void run() {
-		ArrayList<String> list1=new ArrayList<String>();
-		ArrayList<String> list2=new ArrayList<String>();
-		
-		list1.add("bla");
-		list1.add("blu");
-		
-		list2.add("bla");
-
-		list2.add("blu");
-				
-		if(list1.equals(list2)) {
-			println("vasha");
+		try {
+			BufferedReader file = new BufferedReader(new FileReader("fx.java"));
+			String line = file.readLine();
+			while(true) {
+			if(line==null)break;
+			println(line);
+			line=file.readLine();
+			
+			}
+			file.close();
+		} catch (IOException ex) {
+			throw new ErrorException(ex);
 		}
 		
 	}
