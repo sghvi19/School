0cdc719a7ab534348074fba0720ee3dd6ebf4c54
S <s>
2021-02-02 14:41:21 +0400

    {"types":"C","runs":44}

--- workout.java
+++ workout.java
@@ -1,4 +1,23 @@
+import acm.program.ConsoleProgram;
 
-public class workout {
+public class workout extends ConsoleProgram{
 
+
+	public void run() {
+		int arr[] = new int[100];
+		
+		for (int i=0; i<10; ) {
+			for (int j=0; j<8; j++) {
+				j++;
+				arr[i+j] = i+2*j;
+				i++;
+			}
+		}
+
+		for (int i=0; i<10; i++) {
+			print(arr[i] + " ");
+		}
+	}
+
+		
 }
