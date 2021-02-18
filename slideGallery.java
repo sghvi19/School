f64324f6dfeb0c2c4cb3043ceafa4f0476245a0a
S <s>
2021-02-18 13:30:03 +0400

    {"types":"C","runs":25}

--- slideGallery.java
+++ slideGallery.java
@@ -61,11 +61,11 @@ public class slideGallery extends GraphicsProgram {
 	private void reNew(int n) {
 		if (n == -1) {
 			for (int i = 0; i < list.size() - 1; i++) {
-				if (list.get(i).getWidth() == SIZE) {
-					list.get(i).setSize(SIZE / 3, SIZE / 3);
-					list.get(i + 1).setSize(SIZE, SIZE);
-
-				}
+//				if (list.get(i).getWidth() == SIZE) {
+//					list.get(i).setSize(SIZE / 3, SIZE / 3);
+//					list.get(i + 1).setSize(SIZE, SIZE);
+//
+//				}
 
 				list.get(i).setLocation(list.get(i).getX() - DISTANCE - list.get(i + 1).getWidth(),
 						getHeight() / 2 - list.get(i).getHeight() / 2);
