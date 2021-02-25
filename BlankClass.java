d3858fc1d693f01fc5ec60270e033a32b0a09fb2
S <s>
2021-02-25 20:01:51 +0400

    {"types":"C","runs":5}

--- BlankClass.java
+++ BlankClass.java
@@ -741,123 +741,125 @@ import acm.program.ConsoleProgram;
 //		return false;
 //	}
 // 1 variant1 amocana 1
-//	private GRect rect;
-//	private int number = 0;
-//	private String num = "0";
-//	private GLabel label;
-//	private int mouseX;
-//	private int counter=0;
-//	public void run() {
-//		addMouseListeners();
-//		draw();
-//	}
-//
-//	private void draw() {
-//		rect = new GRect(getWidth() / 2 - 100, getHeight() / 2 - 100, 200, 200);
-//		add(rect);
-//		label = new GLabel(num);
-//		add(label, getWidth() / 2 - label.getWidth() / 2, getHeight() / 2 - label.getHeight() / 2);
-//	}
-//
-//
-//
-//	public void mouseDragged(MouseEvent e) {
-//	 if (getElementAt(e.getX(), e.getY()) != null) {
-//		 counter++;
-//		if(counter%2==0) {
-//		 mouseX = e.getX();
-//		}
-//		if (number != 0) {
-//			if (mouseX > e.getX()) {
-//				num = "";
-//				number--;
-//				num += number;
-//				remove(label);
-//				label = new GLabel(num);
-//				add(label, getWidth() / 2 - label.getWidth() / 2, getHeight() / 2 - label.getHeight() / 2);
-//			}
-//		} 
-//		if (number != 9) {
-//			if (mouseX < e.getX()) {
-//				num = "";
-//				number++;
-//				num += number;
-//				remove(label);
-//	label = new GLabel(num);
-//			add(label, getWidth() / 2 - label.getWidth() / 2, getHeight() / 2 - label.getHeight() / 2);
-//				 }
-//
-//			}
-//
-//		}
-//}
-//
-//}
-// circle in center changing size;
+
 public class BlankClass extends GraphicsProgram {
-	private GOval oval;
-	private RandomGenerator rgen = RandomGenerator.getInstance();
-	private static final int CIRCLE_RADIUS = 100;
-
-	private double mouseX;
-	private double mouseY;
-	private double radius;
-
-	private int counter;
-
-	public void init() {
-		oval = null;
-		addMouseListeners();
-		radius = CIRCLE_RADIUS;
-		counter = 0;
-	}
-
+	private GRect rect;
+	private int number = 0;
+	private String num = "0";
+	private GLabel label;
+	private int mouseX;
+	private int counter=0;
 	public void run() {
-		if (oval == null) {
-			oval = new GOval(getWidth() / 2.0 - CIRCLE_RADIUS, getHeight() / 2.0 - CIRCLE_RADIUS, 2 * CIRCLE_RADIUS,
-					2 * CIRCLE_RADIUS);
-			oval.setFilled(true);
-			oval.setFillColor(Color.RED);
-			add(oval);
-		}
+		addMouseListeners();
+		draw();
 	}
 
-	public void mouseClicked(MouseEvent e) {
-		if (getElementAt(e.getX(), e.getY()) == oval) {
-			oval.setFillColor(getRandomColor());
-		}
+	private void draw() {
+		rect = new GRect(getWidth() / 2 - 100, getHeight() / 2 - 100, 200, 200);
+		add(rect);
+		label = new GLabel(num);
+		add(label, getWidth() / 2 - label.getWidth() / 2, getHeight() / 2 - label.getHeight() / 2);
 	}
 
-	public void mousePressed(MouseEvent e) {
-		mouseX = e.getX();
-		mouseY = e.getY();
-	}
 
-	private Color getRandomColor() {
-		return rgen.nextColor();
-	}
 
 	public void mouseDragged(MouseEvent e) {
-		double xCentre = getWidth() / 2.0;
-		double yCentre = getHeight() / 2.0;
-		counter++;
-
-		if (counter % 2 == 0) {
-			mouseX = e.getX();
-			mouseY = e.getY();
+	 if (getElementAt(e.getX(), e.getY()) != null) {
+		 counter++;
+		if(counter%2==0) {
+		 mouseX = e.getX();
 		}
+		if (number != 0) {
+			if (mouseX > e.getX()) {
+				num = "";
+				number--;
+				num += number;
+				remove(label);
+				label = new GLabel(num);
+				add(label, getWidth() / 2 - label.getWidth() / 2, getHeight() / 2 - label.getHeight() / 2);
+			}
+		} 
+		if (number != 9) {
+			if (mouseX < e.getX()) {
+				num = "";
+				number++;
+				num += number;
+				remove(label);
+	label = new GLabel(num);
+			add(label, getWidth() / 2 - label.getWidth() / 2, getHeight() / 2 - label.getHeight() / 2);
+				 }
 
-		double startDistance = getDistance(xCentre, yCentre, mouseX, mouseY);
-		double endDistance = getDistance(xCentre, yCentre, e.getX(), e.getY());
+			}
 
-		double diff = endDistance - startDistance;
-
-		double newRadius = radius + diff;
-		radius = newRadius;
-		oval.setBounds(xCentre - newRadius, yCentre - newRadius, 2 * newRadius, 2 * newRadius);
-	}
-
-	private double getDistance(double x1, double y1, double x2, double y2) {
-		return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-	}
+		}
 }
+
+}
+// circle in center changing size;
+//public class BlankClass extends GraphicsProgram {
+//	private GOval oval;
+//	private RandomGenerator rgen = RandomGenerator.getInstance();
+//	private static final int CIRCLE_RADIUS = 100;
+//
+//	private double mouseX;
+//	private double mouseY;
+//	private double radius;
+//
+//	private int counter;
+//
+//	public void init() {
+//		oval = null;
+//		addMouseListeners();
+//		radius = CIRCLE_RADIUS;
+//		counter = 0;
+//	}
+//
+//	public void run() {
+//		if (oval == null) {
+//			oval = new GOval(getWidth() / 2.0 - CIRCLE_RADIUS, getHeight() / 2.0 - CIRCLE_RADIUS, 2 * CIRCLE_RADIUS,
+//					2 * CIRCLE_RADIUS);
+//			oval.setFilled(true);
+//			oval.setFillColor(Color.RED);
+//			add(oval);
+//		}
+//	}
+//
+//	public void mouseClicked(MouseEvent e) {
+//		if (getElementAt(e.getX(), e.getY()) == oval) {
+//			oval.setFillColor(getRandomColor());
+//		}
+//	}
+//
+//	public void mouseMoved(MouseEvent e) {
+//		mouseX = e.getX();
+//		mouseY = e.getY();
+//	}
+//
+//	private Color getRandomColor() {
+//		return rgen.nextColor();
+//	}
+//
+//	public void mouseDragged(MouseEvent e) {
+//		double xCentre = getWidth() / 2.0;
+//		double yCentre = getHeight() / 2.0;
+//		counter++;
+//
+//		if (counter % 2 == 0) {
+//			mouseX = e.getX();
+//			mouseY = e.getY();
+//		}
+//
+//		double startDistance = getDistance(xCentre, yCentre, mouseX, mouseY);
+//		double endDistance = getDistance(xCentre, yCentre, e.getX(), e.getY());
+//
+//		double diff = endDistance - startDistance;
+//
+//		double newRadius = radius + diff;
+//		radius = newRadius;
+//		oval.setBounds(xCentre - newRadius, yCentre - newRadius, 2 * newRadius, 2 * newRadius);
+//	}
+//
+//	private double getDistance(double x1, double y1, double x2, double y2) {
+//		return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+//	}
+//}
