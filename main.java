41bff59afd8424d847d108f293a554a63c25c4fa
S <s>
2021-02-28 03:38:06 +0400

    {"types":"C","runs":2}

--- main.java
+++ main.java
@@ -1,15 +1,29 @@
 import acm.graphics.GLine;
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class main extends GraphicsProgram {
 
 	public void run() {
-		for(int i=0; i<10; i++) {
-		GLine line1 =new GLine(0,i*getHeight()/10,getWidth(),i*getHeight()/10);
-		add(line1);
-		GLine line2=new GLine(i*getWidth()/10,0, i*getWidth()/10, getHeight());
-		add(line2);
-		}
-	}
 
+		double sqSize = (double) getHeight() / 8;
+
+		for (int i = 0; i < 8; i++) {
+
+			for (int j = 0; j < 8; j++) {
+
+				double x = j * sqSize;
+
+				double y = i * sqSize;
+
+				GRect sq = new GRect(x, y, sqSize, sqSize);
+
+				sq.setFilled((i + j) % 2 != 0);
+
+				add(sq);
+
+			}
+		}
+
+	}
 }
