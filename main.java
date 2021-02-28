99b69c8f29fe4769813d3c883a1f7dfeafb9db05
S <s>
2021-02-28 18:43:16 +0400

    {"types":"C","runs":65}

--- main.java
+++ main.java
@@ -19,7 +19,7 @@ public class main extends GraphicsProgram {
 		addMouseListeners();
 		while (true) {
 			if (!t) {
-				oval.move(5, y);
+				oval.move(0, y);
 				pause(10);
 				if (oval.getY() <= startY+startY/2  && y<0) {
 					startY+=startY/2;
