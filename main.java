7b2097125e6caff99f3e54b3898eac8d29d794d1
S <s>
2021-02-28 18:37:21 +0400

    {"types":"C","runs":51}

--- main.java
+++ main.java
@@ -21,7 +21,7 @@ public class main extends GraphicsProgram {
 			if (!t) {
 				oval.move(0, y);
 				pause(10);
-				if (oval.getY() <= startY*2  && y<0) {
+				if (oval.getY() <= startY+20  && y<0) {
 					y = -y;
 				}
 				if (oval.getY() + 40 >= getHeight()) {
