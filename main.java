cd6031898fc4e08d6010b63c327bc2368f7b50dd
S <s>
2021-02-28 17:44:46 +0400

    {"types":"C","runs":23}

--- main.java
+++ main.java
@@ -12,20 +12,27 @@ public class main extends GraphicsProgram {
 	private int mouseX;
 	private int mouseY;
 	private GLine line;
-
+	private boolean t=true;
 	public void run() {
 		addMouseListeners();
 	}
 
 	public void mouseClicked(MouseEvent e) {
+		if(t) {
 		mouseX = e.getX();
 		mouseY = e.getY();
 		line = new GLine(mouseX, mouseY, mouseX, mouseY);
 		add(line);
+		t=false;
+		}else {
+			line = new GLine(mouseX, mouseY, e.getX(),e.getY());
+			add(line);
+			t=true;
+		}
 	}
 
 	public void mouseMoved(MouseEvent e) {
-		if (line != null) {
+		if (line != null && t) {
 			remove(line);
 			line = new GLine(mouseX, mouseY, e.getX(), e.getY());
 			add(line);
