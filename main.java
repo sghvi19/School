3b017d39bf33b02e3458fbbdc74aee8dc6db3899
S <s>
2021-02-28 20:14:24 +0400

    {"types":"C","runs":78}

--- main.java
+++ main.java
@@ -4,19 +4,28 @@ import acm.graphics.GLine;
 import acm.graphics.GObject;
 import acm.graphics.GOval;
 import acm.graphics.GPoint;
+import acm.graphics.GPolygon;
 import acm.graphics.GRect;
 import acm.program.ConsoleProgram;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
-public class main extends ConsoleProgram {
+public class main extends GraphicsProgram {
 
 	public void run() {
-		String str1=readLine();
-		String str2=readLine();
-		
-		println(str1.compareTo(str2));
-		
+		GPolygon diamond = createDiamond(100, 75);
+		diamond.setFilled(true);
+		diamond.setFillColor(Color.RED);
+		add(diamond, getWidth() / 2, getHeight() / 2);
+	}
+
+	private GPolygon createDiamond(double width, double height) {
+		GPolygon diamond = new GPolygon();
+		diamond.addVertex(-width / 2, 0);
+		diamond.addVertex(0, -height / 2);
+		diamond.addVertex(width / 2, 0);
+		diamond.addVertex(0, height / 2);
+		return diamond;
 	}
 
 }
