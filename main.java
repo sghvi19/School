1de2cc0fa93fa63c072145aa816f194c684da1d3
S <s>
2021-02-28 16:18:10 +0400

    {"types":"C","runs":11}

--- main.java
+++ main.java
@@ -6,17 +6,15 @@ import acm.program.GraphicsProgram;
 public class main extends ConsoleProgram {
 
 	public void run() {
-		int n = readInt("enter ");
-		int result = fibonachi(n);
-		println(result);
+		int x = 0;
+		while (true) {
+			x += 6;
+			if (6 % x == 0 && 8 % x == 0) {
+				println(x);
+				break;
+			}
 
-	}
-
-	private int fibonachi(int n) {
-		if (n == 1 || n == 2) {
-			return 1;
 		}
-		return fibonachi(n-1)+fibonachi(n-2);
 	}
 
 }
