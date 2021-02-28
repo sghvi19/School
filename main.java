219cdab5030433d92eed687efead17f1f92ddcf2
S <s>
2021-02-28 17:40:25 +0400

    {"types":"C","runs":21}

--- main.java
+++ main.java
@@ -9,41 +9,25 @@ import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
 public class main extends GraphicsProgram {
-	private static final int RECT_WIDTH = 80;
-	private static final int RECT_HEIGHT = 100;
-	private static final int MIN_H = 10;
-	private RandomGenerator rg;
-	private GOval oval;
-	private GPoint p;
-	private boolean isPressed = false;
-	
+	private int mouseX;
+	private int mouseY;
+	private GLine line;
 
 	public void run() {
-		rg = RandomGenerator.getInstance();
-		oval = new GOval(getWidth() / 2 - RECT_WIDTH / 2,
-							getHeight() / 2 - RECT_HEIGHT / 2, 
-						RECT_WIDTH, RECT_HEIGHT);
-		oval.setFilled(true);
-		add(oval);
 		addMouseListeners();
 	}
-	
+
 	public void mouseClicked(MouseEvent e) {
-		GObject o = getElementAt(e.getX(), e.getY());
-		if(o != null) {
-			oval.setColor(rg.nextColor());
-		}
+		mouseX = e.getX();
+		mouseY = e.getY();
+		line = new GLine(mouseX, mouseY, mouseX, mouseY);
+		add(line);
 	}
-	public void mousePressed(MouseEvent e) {
-		p = new GPoint(e.getX(), e.getY());
+
+	public void mouseMoved(MouseEvent e) {
+		remove(line);
+		line = new GLine(mouseX, mouseY, e.getX(),e.getY());
+		add(line);
 	}
-	
-	
-	public void mouseDragged(MouseEvent e) {
-		double diffX = e.getX() - p.getX();
-		double diffY = e.getY()- p.getY();
-		oval.setSize(oval.getWidth() + 2*diffX, oval.getHeight() + 2*diffY);
-		oval.setLocation(oval.getX() - diffX, oval.getY() - diffY);
-		p.setLocation(e.getX(), e.getY());
-	}
+
 }
