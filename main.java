9fa2de26697cab780d3b9c152af4cb55f8037dbc
S <s>
2021-02-28 18:20:51 +0400

    {"types":"C","runs":40}

--- main.java
+++ main.java
@@ -11,14 +11,13 @@ import acm.util.RandomGenerator;
 public class main extends GraphicsProgram {
 	private GOval oval;
 	private boolean t = true;
-
+	private double startY;
 	public void run() {
 		int y = 3;
 
 		addMouseListeners();
 		while (true) {
 			if (!t) {
-				double startY = oval.getY();
 				oval.move(0, y);
 				pause(10);
 				if (oval.getY() + 40 >= getHeight()) {
@@ -37,6 +36,7 @@ public class main extends GraphicsProgram {
 			oval = new GOval(e.getX() - 20, e.getY() - 20, 40, 40);
 			add(oval);
 			t = false;
+		startY = oval.getY();
 		} else {
 			oval.setLocation(e.getX() - 20, e.getY() - 20);
 		}
