8930a7694fcaa339a80ca6a36276c6825af28e72
S <s>
2021-02-28 18:49:47 +0400

    {"types":"C","runs":68}

--- main.java
+++ main.java
@@ -9,35 +9,16 @@ import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
 public class main extends GraphicsProgram {
-	private GOval oval;
-	private boolean t = true;
-	private double startY;
-	public void run() {
-		int y = 3;
-		oval = new GOval(getWidth()/2 - 20,getHeight()/2 - 20, 40, 40);
-		add(oval);
-		addMouseListeners();
-		while (true) {
-			if (!t) {
-				oval.move(0, y);
-				pause(10);
-				if (oval.getY() <= startY+startY/5  && y<0) {
-					startY+=startY/5;
-					y = -y;
-				}
-				if (oval.getY() + 40 >= getHeight()) {
-					y = -y;
-				}
-				
-			}
-		}
-	}
 
-	public void mouseClicked(MouseEvent e) {
-		oval.setLocation(e.getX(),e.getY());
-		startY=oval.getY();
-		t=false;
-		
+	public void run() {
+		String str = readLine();
+		String s="";
+		for (int i = str.length()-1; i >= 0; i--) {
+			s+=str.charAt(i);
+		}
+		if(s.equals(str)) {
+			println("abara");
+		}
 	}
 
 }
