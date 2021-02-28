1d0023bf2b0f948465872dd24d655e1d8fdcb399
S <s>
2021-02-28 16:28:10 +0400

    {"types":"C","runs":15}

--- main.java
+++ main.java
@@ -10,6 +10,7 @@ public class main extends GraphicsProgram {
 	private static final int RADIUS=20;
 	public void run() {
 		GOval oval=new GOval(getWidth()/2-RADIUS,getHeight()/2-RADIUS, 2*RADIUS,2*RADIUS);
+		oval.setFilled(true);
 		add(oval);
 		double x=rgen.nextDouble(1,3);
 		double y=1;
