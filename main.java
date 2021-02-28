22c9f448f000df7b8a5f3a2867428f7c448a671c
S <s>
2021-02-28 18:04:03 +0400

    {"types":"C","runs":29}

--- main.java
+++ main.java
@@ -10,16 +10,18 @@ import acm.util.RandomGenerator;
 
 public class main extends GraphicsProgram {
 	private GOval oval;
+	private boolean t=true;
 
 	public void run() {
-		oval = new GOval(getWidth() / 2 - 20, getHeight() / 2 - 20, 40, 40);
-		add(oval);
 		addMouseListeners();
 	}
 
-	public void mouseDragged(MouseEvent e) {
-		if (getElementAt(e.getX(), e.getY()) != null) {
-			oval.setLocation(e.getX()-20,e.getY()-20);
+	public void mouseClicked(MouseEvent e) {
+		if(t) {
+		oval = new GOval(e.getX()- 20, e.getY() - 20, 40, 40);
+		add(oval);
+		}else {
+			oval.setLocation(e.getX()- 20, e.getY() - 20);
 		}
 	}
 
