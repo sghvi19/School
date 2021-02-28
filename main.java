a0a168a5ab6e31e6ab8d189d52ce5d5056807e51
S <s>
2021-02-28 22:57:42 +0400

    {"types":"C","runs":99}

--- main.java
+++ main.java
@@ -11,67 +11,75 @@ import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
 public class main extends GraphicsProgram {
-	private RandomGenerator rgen = RandomGenerator.getInstance();
-	private boolean t1 = true;
-	private boolean t2 = true;
-	private GOval currentOval;
-	private GOval previous = null;
 
 	public void run() {
-		GOval oval;
-		for (int i = 0; i < 25; i++) {
-			Color color = rgen.nextColor();
-			for (int j = 0; j < 2; j++) {
-				int x = rgen.nextInt(0, getWidth() - 40);
-				int y = rgen.nextInt(0, getHeight() - 40);
-				oval = new GOval(x, y, 40, 40);
-				oval.setFilled(true);
-				oval.setColor(color);
-				add(oval);
-			}
-		}
-		addMouseListeners();
-
-		while (true) {
-			pause(2000);
-			Color color = rgen.nextColor();
-			for (int i = 0; i < 2; i++) {
-				int x = rgen.nextInt(0, getWidth() - 40);
-				int y = rgen.nextInt(0, getHeight() - 40);
-				oval = new GOval(x, y, 40, 40);
-				oval.setFilled(true);
-				oval.setColor(color);
-				add(oval);
-			}
-		}
-	}
-
-	public void mouseClicked(MouseEvent e) {
-		if (t1) {
-			currentOval = (GOval) getElementAt(e.getX(), e.getY());
-			t1 = false;
-			if (currentOval != null && previous != null && currentOval != previous) {
-				if (previous != null && currentOval.getColor() == previous.getColor()) {
-					remove(currentOval);
-					remove(previous);
-					t1 = true;
-				}
-			}
-		} else {
-			previous = currentOval;
-			currentOval = (GOval) getElementAt(e.getX(), e.getY());
-			if (currentOval != null && previous != null && currentOval != previous) {
-				if (previous.getColor() == currentOval.getColor()) {
-					remove(previous);
-					remove(currentOval);
-					t1 = true;
-				}
-			} else {
-				previous = currentOval;
-			}
-
-		}
-
+		
 	}
 
 }
+
+//public class main extends GraphicsProgram {
+//	private RandomGenerator rgen = RandomGenerator.getInstance();
+//	private boolean t1 = true;
+//	private boolean t2 = true;
+//	private GOval currentOval;
+//	private GOval previous = null;
+//
+//	public void run() {
+//		GOval oval;
+//		for (int i = 0; i < 25; i++) {
+//			Color color = rgen.nextColor();
+//			for (int j = 0; j < 2; j++) {
+//				int x = rgen.nextInt(0, getWidth() - 40);
+//				int y = rgen.nextInt(0, getHeight() - 40);
+//				oval = new GOval(x, y, 40, 40);
+//				oval.setFilled(true);
+//				oval.setColor(color);
+//				add(oval);
+//			}
+//		}
+//		addMouseListeners();
+//
+//		while (true) {
+//			pause(2000);
+//			Color color = rgen.nextColor();
+//			for (int i = 0; i < 2; i++) {
+//				int x = rgen.nextInt(0, getWidth() - 40);
+//				int y = rgen.nextInt(0, getHeight() - 40);
+//				oval = new GOval(x, y, 40, 40);
+//				oval.setFilled(true);
+//				oval.setColor(color);
+//				add(oval);
+//			}
+//		}
+//	}
+//
+//	public void mouseClicked(MouseEvent e) {
+//		if (t1) {
+//			currentOval = (GOval) getElementAt(e.getX(), e.getY());
+//			t1 = false;
+//			if (currentOval != null && previous != null && currentOval != previous) {
+//				if (previous != null && currentOval.getColor() == previous.getColor()) {
+//					remove(currentOval);
+//					remove(previous);
+//					t1 = true;
+//				}
+//			}
+//		} else {
+//			previous = currentOval;
+//			currentOval = (GOval) getElementAt(e.getX(), e.getY());
+//			if (currentOval != null && previous != null && currentOval != previous) {
+//				if (previous.getColor() == currentOval.getColor()) {
+//					remove(previous);
+//					remove(currentOval);
+//					t1 = true;
+//				}
+//			} else {
+//				previous = currentOval;
+//			}
+//
+//		}
+//
+//	}
+//
+//}
