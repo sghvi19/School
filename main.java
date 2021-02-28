e67926d6554033a17aeb61d4e1aaa064e4e06246
S <s>
2021-02-28 16:18:39 +0400

    {"types":"C","runs":12}

--- main.java
+++ main.java
@@ -9,7 +9,7 @@ public class main extends ConsoleProgram {
 		int x = 0;
 		while (true) {
 			x += 6;
-			if (6 % x == 0 && 8 % x == 0) {
+			if (x%6 == 0 && x%8 == 0) {
 				println(x);
 				break;
 			}
