b7cb6b88c02107d3cb060d125cad8f564d88f095
S <s>
2021-03-01 23:08:40 +0400

    {"types":"C","runs":153}

--- main.java
+++ main.java
@@ -32,9 +32,9 @@ public class main extends ConsoleProgram {
 				result = 1;
 			}
 		}
-//		if (str.charAt(str.length() - 2) != str.charAt(st.length() - 1)) {
-//			st+=str.charAt(str.length()-1);
-//		}
+		if (str.charAt(str.length() - 2) != str.charAt(st.length() - 1)) {
+			st+=str.charAt(str.length()-1);
+		}
 		println(st);
 
 	}
