4ab960a27f7cd8c0165b26545532a769b057db12
S <s>
2021-03-02 23:42:15 +0400

    {"types":"C","runs":1}

--- main.java
+++ main.java
@@ -15,25 +15,18 @@ import acm.util.RandomGenerator;
 public class main extends ConsoleProgram {
 
 	public void run() {
-		int x = 2;
-		int y = 3;
-		GOval o = new GOval(4, 3); // width, height
-		println(racxa(x, 2, o));
-		println(racxa(y, x / 2 + x, o));
-	}
+		double n = 6.25;
+		double x1 = (n + 1) / 2;
+		while (x1 * x1 != n) {
 
-	public int racxa(int x, int y, GOval o) {
-		String s = "hello world";
-		x = varesiRacxa(x / y, x + (int) o.getHeight(), s);
-		o.scale(2);
-		y = varesiRacxa(x, y + (int) o.getHeight(), s);
-		return x + y;
-	}
-
-	public int varesiRacxa(double x, int y, String s) {
-		int z = (int) x + s.charAt(0) - s.charAt(1);
-		s = s.substring(1);
-		return z + y;
+			if (x1 * x1 > n) {
+				x1 = (x1 + 1) / 2;
+			}
+			if(x1*x1<n) {
+				x1= (x1+n)/2;
+			}
+		}
+		println(x1);
 	}
 
 }
