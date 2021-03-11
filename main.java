e5b0a23dbc67be013364e07d677d6d0eea06c3f5
S <s>
2021-03-11 21:29:32 +0400

    {"types":"C","runs":21}

--- main.java
+++ main.java
@@ -12,7 +12,77 @@ import acm.program.ConsoleProgram;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
-public class main extends GraphicsProgram {
+public class main extends ConsoleProgram {
+	public void run() {
+		int[][] matrix = new int[4][8];
+		
+		int[] arr = {0,1,1,0,0,1,0,0};
+		int[] arr1 = {0,1,1,1,1,1,0,0};
+		int[] arr2 = {0,0,1,1,1,1,0,0};
+		int[] arr3 = {0,0,1,1,1,1,0,0};
+		
+		matrix[0] = arr;
+		matrix[1] = arr1;
+		matrix[2] = arr2;
+		matrix[3] = arr3;
+		
+		println(maxRectangleArea(matrix));
+	}
+	
+	// Problem: 59
+	private int maxRectangleArea(int[][] matrix) {
+		int maxArea = 0;
+		
+		for(int i = 0; i < matrix.length; i++) {
+			
+			for(int j = 0; j < matrix[i].length; j++) {
+				
+				if(matrix[i][j] == 1) {
+					int area = maxSubRectangleArea(matrix, i, j);
+					if(area > maxArea) {
+						maxArea = area;
+					}
+				}
+			}
+			
+		}
+		return maxArea;
+	}
+	
+	private int maxSubRectangleArea(int[][] matrix, int iStart, int jStart) {
+		int maxArea = 0;
+		
+		for(int iEnd = iStart; iEnd < matrix.length; iEnd++) {
+			
+			for(int jEnd = jStart; jEnd < matrix[0].length; jEnd++) {
+				
+				int currentArea = rectangeArea(matrix, iStart, jStart, iEnd, jEnd);
+				
+				if( currentArea != -1) {
+					if(maxArea < currentArea) {
+						maxArea = currentArea;
+					}
+				}
+			}
+		}
+		return maxArea;
+	}
+	
+	
+	private int rectangeArea(int[][] matrix, int iStart, int jStart, int iEnd, int jEnd) {
+		int area = 0;
+		
+		for(int i = iStart; i <= iEnd; i++) {
+			for(int j = jStart; j <= jEnd; j++) {
+				
+				if(matrix[i][j] == 0) {
+					return -1;
+				}
+				area ++;
+			}
+		}
+		return area;
+	}
 		
 }
 
