2cf54eb704785b6b1fd188b20277193ec1292604
S <s>
2022-04-13 19:56:36 +0400

    {"types":"C","runs":0}

--- School.java
+++ School.java
@@ -1,4 +1,70 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public class School {
-
+	private Map<String, HashMap<String,ArrayList<String> > > data;
+	
+	
+	public School() {
+		 data = new  HashMap<>();
+	}
+	
+	
+	public void addTeacher(String teacher) {
+		if(!data.containsKey(teacher)) data.put(teacher, new HashMap<>());
+	}
+	
+	public void addSubject(String teacher, String subject) {
+		data.get(teacher).put(subject,new ArrayList<>());
+	}
+	
+	public void addPupil(String pupil, String subject) {
+		for(HashMap<String, ArrayList<String>> map : data.values() ) {
+			if(map.containsKey(subject)) {
+				map.get(subject).add(pupil);
+			}
+		}
+		
+	}
+	
+	public Iterator getTeachers(String pupil) {
+		List<String> teachers = new ArrayList<String>(); 
+		
+		for(String t: data.keySet()) {
+			HashMap<String, ArrayList<String>> subPups = data.get(t); 
+			for(ArrayList<String> pups: subPups.values()) {
+				if(pups.contains(pupil)) teachers.add(t);
+			}
+			
+		}
+		
+		return teachers.iterator();
+		
+	}
+	
+	public Iterator getPupils(String teacher) {
+		ArrayList<String> pups = new ArrayList<String>(); 
+		
+		for(String sub: data.get(teacher).keySet()) {
+			
+				 pups.addAll(data.get(teacher).get(sub));
+		}
+		
+		return pups.iterator();
+			
+	}
+	
+	
+	public void removeTeacher(String teacher) {
+		data.remove(teacher);
+	}
+	
+	
+	
+	
+	
+	
 }
