cd3a2396a899c8b301371facddc83d67ed889b6a
S <s>
2022-04-13 20:15:09 +0400

    {"types":"C","runs":1}

--- Graphic.java
+++ Graphic.java
@@ -1,4 +1,11 @@
+import javax.swing.JButton;
 
-public class Graphic {
+import acm.program.GraphicsProgram;
+import acmx.export.javax.swing.JFrame;
 
+public class Graphic extends JFrame{
+		public static void main(String[] args) {
+			
+		}
+		
 }
