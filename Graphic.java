0430410ac4d2eebac512a4dd9492e378fdcfce0a
S <s>
2022-04-14 19:47:10 +0400

    {"types":"C","runs":48}

--- Graphic.java
+++ Graphic.java
@@ -9,143 +9,118 @@ import java.awt.event.ActionEvent;
 import java.util.Iterator;
 
 
-public class Graphic extends GraphicsProgram {
-	// Constants
-	private final int RESULT_X = 10;
-	private final int LINE_H = 20;
+public class Graphic extends GraphicsProgram{
+	private static final int distance = 5;
+	
+	
+	
+	private JTextField teaField;
+	private JTextField subField;
+	private JTextField pupField;
+	
+	private JButton addT;
+	private JButton addP;
+	private JButton addS;
+	private JButton disP;
+	private JButton disT;
+	
+	
+	
+	
+	private School school;
+		
+		public  void init() {
+            school  = new School();
+            
+            this.setSize(1300, 600);
+			
+			addT = new JButton("Add Teach");
+			addP = new JButton("Add Pupil");
+			addS = new JButton("Add Subj");
+			disP = new JButton("Display Pupils");
+			disT = new JButton("Display Teachers");
+			
+			JLabel tea = new JLabel("Teach");
+			JLabel subj = new JLabel("Subj");
+			JLabel pup = new JLabel("Pupil");
+			
+			teaField = new JTextField(10);
+			subField = new JTextField(10);
+			pupField = new JTextField(10);
+			
+			add(tea,SOUTH);
+			add(teaField,SOUTH);
+			add(subj,SOUTH);
+			add(subField,SOUTH);
+			add(pup,SOUTH);
+			add(pupField,SOUTH);
+			add(addT,SOUTH);
+			addT.addActionListener(this);
+			add(addS,SOUTH);
+			addS.addActionListener(this);
+			add(addP,SOUTH);
+			addP.addActionListener(this);
+			add(disP,SOUTH);
+			disP.addActionListener(this);
+			add(disT,SOUTH);
+			disT.addActionListener(this);
+			
+			addActionListeners();
+			
 
-	// School
-	School school;
-
-	// Labels
-	JLabel teachLabel;
-	JLabel subjLabel;
-	JLabel pupilLabel;
-
-	// TextFields
-	JTextField teachTextField;
-	JTextField subjTextField;
-	JTextField pupilTextField;
-
-	// Buttons
-	JButton addTeachButton;
-	JButton addSubjButton;
-	JButton addPupilButton;
-	JButton displayPupilsButton;
-	JButton displayTeachersButton;
-
-	@Override
-	public void init() {
-		school = new School();
-//		school = new RefactoredÏSchool();
-
-		// Increase size (by default some components will be trimmed and not visible)
-		this.setSize(1200, 800);
-
-		// Initialize Labels
-		teachLabel = new JLabel("Teach:");
-		subjLabel = new JLabel("Subj:");
-		pupilLabel = new JLabel("Pupil:");
-
-		// Initialize TextFields
-		teachTextField = new JTextField(10);
-		subjTextField = new JTextField(10);
-		pupilTextField = new JTextField(10);
-
-		// Initialize Buttons
-		addTeachButton = new JButton("Add Teach");
-		addSubjButton = new JButton("Add Subj");
-		addPupilButton = new JButton("Add Pupil");
-		displayPupilsButton = new JButton("Display Pupils");
-		displayTeachersButton = new JButton("Display Teachers");
-
-		// Add Labels with corresponding TextFields
-		add(teachLabel, SOUTH);
-		add(teachTextField, SOUTH);
-		add(subjLabel, SOUTH);
-		add(subjTextField, SOUTH);
-		add(pupilLabel, SOUTH);
-		add(pupilTextField, SOUTH);
-
-		// Add Buttons
-		add(addTeachButton, SOUTH);
-		add(addSubjButton, SOUTH);
-		add(addPupilButton, SOUTH);
-		add(displayPupilsButton, SOUTH);
-		add(displayTeachersButton, SOUTH);
-
-		addActionListeners();
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// 1. Evaluate input
-		if (e.getSource() == addTeachButton) {
-			addTeacher();
-		} else if (e.getSource() == addSubjButton) {
-			addSubject();
-		} else if (e.getSource() == addPupilButton) {
-			addPupil();
-		} else if (e.getSource() == displayPupilsButton) {
-			displayPupils();
-		} else if (e.getSource() == displayTeachersButton) {
-			displayTeachers();
+			
+		}
+	
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == addT) {
+				
+				school.addTeacher(teaField.getText());
+				
+			}
+			if(e.getSource() == addP) {
+				school.addPupil(pupField.getText(), subField.getText());
+			}
+			if(e.getSource() == addS) {
+				school.addSubject(teaField.getText(), subField.getText());
+			}
+			if(e.getSource() == disP) {
+				displayPupils();
+			}
+			if(e.getSource() == disT) {
+				displayTeachers();
+			}
+			
+			teaField.setText("");
+			subField.setText("");
+			pupField.setText("");
+		}
+		
+		
+		private void displayTeachers() {
+			removeAll();
+			Iterator<String> it = school.getTeachers(pupField.getText());
+			
+			if(it == null) return;
+			add(new GLabel("Results:", 10, 5));
+			int size = 1;
+			while(it.hasNext()) {
+				println("a");
+				String teacher = it.next();
+				
+				add(new GLabel(teacher, distance , distance*size));
+				size++;
+			}
+		
+			
 		}
 
-		// 2. After All Operations Clear Inputs
-		teachTextField.setText("");
-		subjTextField.setText("");
-		pupilTextField.setText("");
-	}
-
-	private void addTeacher() {
-		String teacherName = teachTextField.getText();
-		school.addTeacher(teacherName);
-	}
-
-	private void addSubject() {
-		String teacherName = teachTextField.getText();
-		String subjectName = subjTextField.getText();
-		school.addSubject(teacherName, subjectName);
-	}
-
-	private void addPupil() {
-		String pupilName = pupilTextField.getText();
-		String subjectName = subjTextField.getText();
-		school.addPupil(pupilName, subjectName);
-	}
-
-	private void displayPupils() {
-		String teacherName = teachTextField.getText();
-		Iterator<String> it = school.getPupils(teacherName);
-		displayContent(it);
-	}
-
-	private void displayTeachers() {
-		String pupilName = pupilTextField.getText();
-		Iterator<String> it = school.getTeachers(pupilName);
-		displayContent(it);
-	}
-
-	private void displayContent(Iterator<String> it) {
-		// 1. Check for iterator
-		if (it == null) {
-			return;
+		private void displayPupils() {
+			removeAll();
+			Iterator<String> it = school.getPupils(teaField.getText());
+			
+			
 		}
-
-		// 2. Clear entire screen
-		removeAll();
-
-		// 3. Add 'Result' label
-		add(new GLabel("Results:", RESULT_X, LINE_H));
-
-		// 4. Iterate with iterator and display all
-		int offset = 2;
-		while (it.hasNext()) {
-			String name = it.next();
-			add(new GLabel(name, RESULT_X, LINE_H * offset));
-			offset++;
-		}
-	}
-
+	
+		
+		
 }
