d3d6dc2cfc633cc45fd673e5d2e95ca5ec722c51
S <s>
2022-04-14 14:32:01 +0400

    {"types":"C","runs":2}

--- Graphic.java
+++ Graphic.java
@@ -1,11 +1,62 @@
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 import acm.program.GraphicsProgram;
 import acmx.export.javax.swing.JFrame;
 
-public class Graphic extends JFrame{
-		public static void main(String[] args) {
+public class Graphic extends GraphicsProgram{
+	JTextField teaField;
+	JTextField subField;
+	JTextField pupField;
+	
+	JButton addT;
+	JButton addP;
+	JButton addS;
+	JButton disP;
+	JButton disT;
+	
+	JLabel tea;
+	JLabel subj;
+	JLabel pup;
+	
+		
+		public  void init() {
+			addT = new JButton("Add Teach");
+			addP = new JButton("Add Pupil");
+			addS = new JButton("Add Subj");
+			disP = new JButton("Display Pupils");
+			disT = new JButton("Display Teachers");
+			
+			tea = new JLabel("Teach");
+			subj = new JLabel("Subj");
+			pup = new JLabel("Pupil");
+			
+			teaField = new JTextField(10);
+			subField = new JTextField(10);
+			pupField = new JTextField(10);
+			
+			add(tea,SOUTH);
+			add(teaField,SOUTH);
+			add(subj,SOUTH);
+			add(subField,SOUTH);
+			add(pup,SOUTH);
+			add(pupField,SOUTH);
+			add(addT,SOUTH);
+			add(addS,SOUTH);
+			add(addP,SOUTH);
+			add(disP,SOUTH);
+			add(disT,SOUTH);
+			
+			addActionListeners();
+			
+
 			
 		}
+		
+		public void run() {
+			
+		}
+		
 		
 }
