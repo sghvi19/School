9cf4a3f4e0ae7d156f1b057e79d66f4465c32474
S <s>
2022-04-14 16:02:20 +0400

    {"types":"C","runs":7}

--- School.java
+++ School.java
@@ -30,7 +30,7 @@ public class School {
 		
 	}
 	
-	public Iterator getTeachers(String pupil) {
+	public Iterator<String> getTeachers(String pupil) {
 		List<String> teachers = new ArrayList<String>(); 
 		
 		for(String t: data.keySet()) {
@@ -45,7 +45,7 @@ public class School {
 		
 	}
 	
-	public Iterator getPupils(String teacher) {
+	public Iterator<String> getPupils(String teacher) {
 		ArrayList<String> pups = new ArrayList<String>(); 
 		
 		for(String sub: data.get(teacher).keySet()) {
