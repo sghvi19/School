95f91b50603adc7bb722d8d9533613c89c3a099c
S <s>
2022-04-14 19:15:13 +0400

    {"types":"C","runs":43}

--- Graphic.java
+++ Graphic.java
@@ -98,14 +98,14 @@ public class Graphic extends GraphicsProgram{
 		
 		private void displayTeachers() {
 			removeAll();
-			Iterator it = school.getTeachers(pupField.getText());
+			Iterator<String> it = school.getTeachers(pupField.getText());
 			
 			if(it == null) return;
 			
 			int size = 1;
 			while(it.hasNext()) {
 				println("a");
-				String teacher = (String) it.next();
+				String teacher = it.next();
 				
 				add(new GLabel(teacher, distance , distance*size));
 				size++;
